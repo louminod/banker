2b934dfa302160e2a74cacfec4b09e0f2092b495
louminod <30729464+louminod@users.noreply.github.com>
2022-01-30 18:25:58 +0100

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/banker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/banker.dir/main.cpp.o"
+  "banker"
+  "banker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/banker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
